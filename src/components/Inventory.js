342beb7a2e2380e674ec3c0606939282c9397fc7
Mark Atherton <mark@insightinternet.co.uk>
2017-10-04 18:30:01 +0100

    first commit got to end of lesson 5

---
+++ src/components/Inventory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Inventory extends React.Component {
+	render() {
+		return(
+			<p>Inventory Component</p>
+		)
+	}
+}
+
+
+export default Inventory;
---
+++ src/components/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Order extends React.Component {
+	render() {
+		return(
+			<p>Order Component</p>
+		)
+	}
+}
+
+
+export default Order;
