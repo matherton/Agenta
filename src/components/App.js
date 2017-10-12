22a8705e317316e99021a957f01eb2cc8bb3e51e
Mark Atherton <mark@insightinternet.co.uk>
2017-10-12 14:46:58 +0100

    created chat veiw

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,19 @@ import Order from './Order';
 class App extends React.Component {
 	render() {
 		return (
-			<span></span>
+			<section className="chat-panel">
+	      <header className="chat-header">
+	        <h1>Chat</h1>
+	      </header>
+	      <div className="message-container">
+	        <div className="message-title">Roberto Lastname</div>
+	        <div className="chat-bubble">chat message goes here</div>
+	        <div className="message-title">Roberto Lastname</div>
+	        <div className="chat-bubble">second chat message, maybe</div>
+	      </div>
+	      <input type="textbox" className="chat-box" placeholder="Your mesage here" />
+	      <button type="submit" className="chat-button chat-orange">Send</button>
+	    </section>
 		)
 	}
 }
